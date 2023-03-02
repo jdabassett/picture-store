cd3321353475dcd876c0797d9820cc101dae75b0
jdabassett <jacobbassett@gmail.com>
2023-03-02 12:15:20 -0700

    house cleaning before start

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h2>Pic Some</h2>
+      <i className=""></i>
+    </header>
+  )
+}
---
+++ src/pages/Photos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Photos() {
+  return (
+    <main className="photos">
+      <h1>Images go here</h1>
+    </main>
+
+  )
+}
